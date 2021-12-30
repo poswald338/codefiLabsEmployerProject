a9dd72d44147d29709334e37184d4ce495923bf3
Patrick Oswald <poswald338@gmail.com>
2021-12-30 10:05:41 -0600

    update showcase component with content

--- src/app/showcase/showcase.component.ts
+++ src/app/showcase/showcase.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Sites } from './static-sites.model';
 
 @Component({
   selector: 'app-showcase',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./showcase.component.scss']
 })
 export class ShowcaseComponent implements OnInit {
+  apps = Sites
 
   constructor() { }
 
---
+++ src/app/showcase/static-sites.model.ts
@@ -0,0 +1,18 @@
+export const Sites = [
+  {
+    name: "IHM Used Parts",
+    img: "assets/img/portfolio/ihm-home-2.png",
+    url: "ihm-web-prod.herokuapp.com"
+  },
+  {
+    name: "CSI Mock Project",
+    img: "assets/img/portfolio/csi-home.png",
+    url: "codelabs-csi.herokuapp.com"
+  },
+  {
+    name: "Movie Madness",
+    img: "assets/img/portfolio/ihm-home-2.png",
+    url: "mymovieapp.com"
+  },
+
+]
