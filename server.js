12f597dc35f78dc6b58f9a88854de52777c8df81
Patrick Oswald <poswald338@gmail.com>
2021-12-31 13:38:49 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 
-// Install Express Server
 const express = require('express');
+const forceSsl = require('force-ssl-heroku');
+const enforce = require('express-sslify');
 const path = require('path');
+const enableProdMode = require('@angular/core').enableProdMode
+const compression = require('compression')
+
+enableProdMode();
+
 const app = express();
-// Server only are stactic files from the dist directory
-app.use(express.static('./dist/now-ui-kit-pro-angular'));
-app.get('/*', (req, res) =>
-res.sendFile('index.html', {root: 'dist/now-ui-kit-pro-angular'}),
-);
-// Tell the app how to start on Heroku
-app.listen(process.env.PORT || 8080);
+
+app.use(compression());
+
+app.use(express.static('./dist'));
+
+app.use(enforce.HTTPS({ trustProtoHeader: true }));
+
+app.get('/*', (req,res)=>{
+    res.sendFile(path.join(__dirname,'/dist/index.html'));
+});
+
+app.listen(process.env.PORT || 8080, ()=>{
+    console.log('Server started')
+})
